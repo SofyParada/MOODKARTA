78f92c1a941bb62fff1f993b72092ef071dd7adc
Guteh <benjamin.gutierrezc@usm.cl>
2024-11-03 20:22:56 -0300

    prueba branch

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -23,7 +23,7 @@ export const NavBar = () => {
       </NavLink>
       <NavLink
         className={navLinkClass}
-        to='/links'
+        to='/apoyo'
       >
         Apoyo
       </NavLink>
