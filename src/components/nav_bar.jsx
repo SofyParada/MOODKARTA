484ffb2077e0b639e8223549224476c2cf655594
Caro Muñoz <136779696+Kropmunoz@users.noreply.github.com>
2024-11-05 19:26:11 -0300

    nav_bar.jsx

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+import Inicio from '../assets/Inicio.png'
+import Contacto from '../assets/contacto.png'
+import recursos from '../assets/recursos.png'
+
 export const NavBar = () => {
   const navLinkClass = ({ isActive }) => {
     const classes = ['nav-bar__link']
@@ -13,19 +17,13 @@ export const NavBar = () => {
         className={navLinkClass}
         to='/'
       >
-        Inicio
+        <img src={Inicio} alt='Inicio' className='nav-bar__icon' />
       </NavLink>
-      <NavLink
-        className={navLinkClass}
-        to='/tendencias'
-      >
-        Tendencias
+      <NavLink className={navLinkClass} to='/tendencias'>
+        <img src={recursos} alt='Tendencias' className='nav-bar__icon' />
       </NavLink>
-      <NavLink
-        className={navLinkClass}
-        to='/apoyo'
-      >
-        Apoyo
+      <NavLink className={navLinkClass} to='/apoyo'>
+        <img src={Contacto} alt='Apoyo' className='nav-bar__icon' />
       </NavLink>
     </nav>
   )
