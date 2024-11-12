121657932d397947e9b8bd1f13f3f95a86e0f41f
Caro Muñoz <136779696+Kropmunoz@users.noreply.github.com>
2024-11-12 12:11:45 -0300

    Add files via upload

--- src/pages/apoyo.jsx
+++ src/pages/apoyo.jsx
@@ -23,11 +23,11 @@ export const Apoyo = () => {
         <div className='apoyo-descripcion-2'>
           <img src={Nube} className="button-icon" alt='Nube' />
           Como lidiar con la tristeza: <br />
-          <a href='https://www.cdc.gov/howrightnow/espanol/recursos/sobrellevando-la-tristeza/index.html' className='texto'> 'https://www.cdc.gov/howrightnow/espanol/recursos/sobrellevando-la-tristeza/index.html' </a>
+          <a href='https://www.cdc.gov/howrightnow/espanol/recursos/sobrellevando-la-tristeza/index.html' className='texto'> 'www.cdc.gov/sobrellevando-la-tristeza/' </a>
           <br /> <br />
           <img src={Bandera} className="button-icon" alt='Nube' />
           Emociones negativas se vinculan con el estrés: <br />
-          <a href='https://www.cdc.gov/howrightnow/espanol/emocion/estres/' className='texto'> 'https://www.cdc.gov/howrightnow/espanol/emocion/estres/' </a>
+          <a href='https://www.cdc.gov/howrightnow/espanol/emocion/estres/' className='texto'> 'www.cdc.gov/emocion/estres/' </a>
 
           <br /> <br /> <br /> <br />
           Recuerda que siempre que tengas problemas, ¡Trata de comunicarte con alguien, <span style={{fontFamily:"Fraunces",fontWeight:"600"}}>no estás solo!</span>
