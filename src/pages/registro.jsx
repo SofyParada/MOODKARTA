198e6d2192b62f6723a668d1cf1f1874ce75c2e7
Guteh <benjamin.gutierrezc@usm.cl>
2024-11-14 16:33:21 -0300

    eval y edit

--- src/pages/registro.jsx
+++ src/pages/registro.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import ticket from '../assets/ticket.png';
 import noRegistrado from '../assets/NOregistrado.png';
 import Izquierda from '../assets/flechaizquierda.png';
+import Edit from '../assets/edit.png';
 
 export const Registro = () => {
   const navigate = useNavigate();
@@ -13,14 +14,14 @@ export const Registro = () => {
   };
 
   const encuestas = [
-    { fecha: '30/10/2024', completado: true, emoji: '😀' },
-    { fecha: '31/10/2024', completado: false, emoji: '-' },
-    { fecha: '1/11/2024', completado: true, emoji: '😞' },
-    { fecha: '2/11/2024', completado: true, emoji: '😢' },
-    { fecha: '3/11/2024', completado: true, emoji: '😠' },
-    { fecha: '4/11/2024', completado: false, emoji: '-' },
-    { fecha: '5/11/2024', completado: true, emoji: '😎' },
-    { fecha: '6/11/2024', completado: false, emoji: '-' },
+    { fecha: '6/11/2024', completado: false, emoji: '-' , ptje: 'Eval: 5'},
+    { fecha: '5/11/2024', completado: true, emoji: '😎' , ptje: 'Eval: 8'},
+    { fecha: '4/11/2024', completado: false, emoji: '-' , ptje: 'Eval: 4'},
+    { fecha: '3/11/2024', completado: true, emoji: '😠' , ptje: 'Eval: 3'},
+    { fecha: '2/11/2024', completado: true, emoji: '😢' , ptje: 'Eval: 2'},
+    { fecha: '1/11/2024', completado: true, emoji: '😞' , ptje: 'Eval: 1'},
+    { fecha: '31/10/2024', completado: false, emoji: '-' , ptje: 'Eval: 7'},
+    { fecha: '30/10/2024', completado: true, emoji: '😀', ptje: 'Eval: 9'},
   ];
 
 
@@ -31,7 +32,7 @@ export const Registro = () => {
       <div className="table-container">
         <table>
           <tbody>
-            {encuestas.map(({ fecha, completado, emoji }, index) => (
+            {encuestas.map(({ fecha, completado, emoji, ptje }, index) => (
               <tr key={index}>
                 <td>
                   <img
@@ -42,6 +43,15 @@ export const Registro = () => {
                 </td>
                 <td>{fecha}</td>
                 <td>{emoji}</td>
+                <td>{ptje}</td>
+                <td>
+                  {<button onClick={() => handleNavigate('/encuesta-diaria')} style={{border:'none', background: 'none', cursor:'pointer'}}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <img src={Edit} height={20} alt='Edit' />
+                    </div>
+                  </button>
+                  } 
+                </td>
               </tr>
             ))}
           </tbody>
